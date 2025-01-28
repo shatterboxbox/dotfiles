2584cb284224f61359642e0c965d3f9a8f8eee7d
Colton Elleray <colton.elleray@outlook.com>
2025-01-28 12:49:12 -0800

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,7 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
 require("config.lazy")
+
